afd4b05eea9cbbd0a5cb283f5cd8e3a57b58b2bd
tigergao99 <46076221+tigergao99@users.noreply.github.com>
2020-04-21 16:09:02 -0400

    Add files via upload

--- cap_socket.c
+++ cap_socket.c
@@ -1,22 +1,24 @@
 /*-
- * SPDX-License-Identifier: BSD-2-Clause-FreeBSD
+ * SPDX-License-Identifier: BSD-2-Clause
  *
- * Copyright (c) 2020
- * All rights reserved.
+ * Copyright (c) 2020 The FreeBSD Foundation
+ *
+ * This software was developed by Tiger Gao under sponsorship from
+ * the FreeBSD Foundation.
  *
  * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
+ * modification, are permitted provided that the following conditions are
+ * met:
  * 1. Redistributions of source code must retain the above copyright
  *    notice, this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the distribution.
  *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHORS AND CONTRIBUTORS ``AS IS'' AND
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
@@ -36,6 +38,8 @@
 
 #include <arpa/inet.h>
 
+#include <err.h>
+#include <netdb.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -46,14 +50,63 @@
 
 #include "cap_socket.h"
 
-int 
-cap_socket_init(cap_channel_t *chan, int count, const char **allowed_dest)
+static nvlist_t *
+parse_and_store_args(const char *dest)
 {
-	nvlist_t *limits;
+	char *args[3];
+	int j;
+	const int len = strlen(dest) + 1;
+	char *token;
+	nvlist_t *nvl;
+
+	j = 0;
+	char buf[len];
+	nvl = nvlist_create(0);
+	strncpy(buf, dest, len);
+	token = strtok(buf, ":");
+	while (token) {
+		args[j++] = token;
+		token = strtok(NULL, ":");
+	}
+	if (j == 3) {
+		nvlist_add_string(nvl, "type", args[0]);
+		nvlist_add_string(nvl, "hostname", args[1]);
+		nvlist_add_string(nvl, "port", args[2]);
+	} else {
+		fprintf(stderr, "%s", "Format error. \n");
+		return (NULL);
+	}
+
+	return nvl;
+}
+
+int 
+cap_socket_init(cap_channel_t *chan, int count, const char **allowed_dests)
+{
+	nvlist_t *limits, *nvl;
+	nvlist_t *arr[count];
+	int i;
 
 	limits = nvlist_create(0);
-	nvlist_add_string_array(limits, "allowed_dest", allowed_dest, count);
+	for(i = 0; i < count; i++) {
+		/* dest is of format "tcp:127.0.0.1:8080", port can be set to any port
+		   using sentinel value -1. If type is left empty it will be resolved
+		   automatically eg. www.google.com:-1
+		*/
+		nvl = parse_and_store_args(allowed_dests[i]);
+		if (!nvl) {
+			err(1, "Wrong format: %s", allowed_dests[i]);
+			return (-1);
+		}
+		arr[i] = nvl;
+	}
+	nvlist_add_nvlist_array(limits, "allowed_dests", 
+	    (const struct nvlist *const *)arr, count);
+	/* free memory */
+	for(i = 0; i < count; i++)
+		nvlist_destroy(arr[i]);
 	if (cap_limit_set(chan, limits) < 0) {
+		fprintf(stderr, "%s", "Cannot set limit. \n");
 		return (-1);
 	}
 	return (0);
@@ -101,13 +154,14 @@ static int
 exec_command(const char *cmd, const nvlist_t *limits, nvlist_t *nvlin,
     nvlist_t *nvlout) 
 {
-	const char * const *allowed_dest;
-	char *buf;
-	char dest[50], ip_addr[50];
+	const nvlist_t * const *allowed_dests;
+	const nvlist_t *allowed_dest;
+	nvlist_t *dest_args;
+	const char *cause, *dest_host, *dest_type, *dest_port;
+	struct addrinfo hints, *res, *res0;
 	size_t nitems;
-	int sock, type, portno;
+	int sock, error, type;
 	unsigned int i;
-	struct sockaddr_in serv_addr; 
 	bool is_allowed;
 
 	if (strcmp(cmd, "socket_connect") != 0)
@@ -116,48 +170,83 @@ exec_command(const char *cmd, const nvlist_t *limits, nvlist_t *nvlin,
 		return (ENOTCAPABLE);
 
 	is_allowed = false;
-	allowed_dest = nvlist_get_string_array(limits, "allowed_dest", &nitems);
-	strcpy(dest, nvlist_get_string(nvlin, "dest"));
-	/* Check if dest addr in allowed set */
+	/* Parse and store current socket destination */
+	dest_args = parse_and_store_args(nvlist_get_string(nvlin, "dest"));
+	dest_host = nvlist_get_string(dest_args, "hostname");
+	dest_type = nvlist_get_string(dest_args, "type");
+	dest_port = nvlist_get_string(dest_args, "port");
+	/* Check if dest in allowed set */
+	allowed_dests = nvlist_get_nvlist_array(limits, "allowed_dests", &nitems);
 	for(i = 0; i < nitems; i++) {
-		if (strcmp(allowed_dest[i], dest) == 0)
-			is_allowed = true;
+		allowed_dest = allowed_dests[i];
+		if (strcmp(nvlist_get_string(allowed_dest, "hostname"), 
+		    dest_host) == 0) {
+				/* type and port must be allowed. If type is any or type is equal to
+				   current type, then type is allowed. If port is -1 or port is equal
+				   to current port, port is allowed.
+				   */
+				const char *allowed_type = nvlist_get_string(allowed_dest, "type");
+				const char *allowed_port = nvlist_get_string(allowed_dest, "port");
+				if (((strcmp(allowed_type, "any") == 0) ||
+				    (strcmp(allowed_type, dest_type) == 0)) &&
+					((strcmp(allowed_port, "-1") == 0) ||
+				    (strcmp(allowed_port, dest_port) == 0)))
+					is_allowed = true;
+		}
 	}
 	if (!is_allowed)
 		return (ENOTCAPABLE);
-
-	/* parse socket type, address, and port */
-	buf = strtok(dest, ":");
-	if (!strcmp(buf, "tcp") || !strcmp(buf, "TCP"))
+	/* initialize hints */
+	memset(&hints, 0, sizeof(hints));
+	/* Set socket type hint if type is specified. */
+	type = -1;
+	if (!strcmp(dest_type, "tcp") || !strcmp(dest_type, "TCP"))
 		type = SOCK_STREAM;
-	else if (!strcmp(buf, "udp") || !strcmp(buf, "UDP"))
+	else if (!strcmp(dest_type, "udp") || !strcmp(dest_type, "UDP"))
 		type = SOCK_DGRAM;
+	else if (!strcmp(dest_type, "raw") || !strcmp(dest_type, "RAW"))
+		type = SOCK_RAW;
+	else if (!strcmp(dest_type, "rdm") || !strcmp(dest_type, "RDM"))
+		type = SOCK_RDM;
+	else if (!strcmp(dest_type, "seqpacket") || 
+	    !strcmp(dest_type, "SEQPACKET"))
+		type = SOCK_SEQPACKET;
 	else {
-		nvlist_add_string(nvlout, "errmsg", "Please specify tcp or udp.\n");
+		nvlist_add_string(nvlout, "errmsg", "Wrong socket type.\n");
 		return (EINVAL);
 	}
-	if ((sock = socket(AF_INET, type, 0)) < 0) { 
-        nvlist_add_string(nvlout, "errmsg", "Socket creation.\n");
+	if (type != -1)
+		hints.ai_socktype = type;
+	hints.ai_flags = AI_ALL;
+	if (!strcmp(dest_port, "-1"))
+		dest_port = NULL;
+	error = getaddrinfo(dest_host, dest_port, &hints, &res0);
+	if (error) {
+		nvlist_add_string(nvlout, "errmsg", gai_strerror(error));
 		return (EINVAL);
-    }
-	buf = strtok(NULL, ":");
-	strcpy(ip_addr, buf);
-	buf = strtok(NULL, ":");
-	portno = atoi(buf);
-	serv_addr.sin_family = AF_INET; 
-    serv_addr.sin_port = htons(portno);
-	if (inet_pton(AF_INET, ip_addr, &serv_addr.sin_addr) <= 0)  
-    {
-        nvlist_add_string(nvlout, "errmsg", 
-		    "Invalid address/ Address not supported \n");
+	}
+	for (res = res0; res; res = res->ai_next) {
+		sock = socket(res->ai_family, res->ai_socktype,
+			res->ai_protocol);
+		if (sock < 0) {
+			cause = "socket\n";
+			continue;
+		}
+
+		if (connect(sock, res->ai_addr,	res->ai_addrlen) < 0) {
+			cause = "connect\n";
+			close(sock);
+			sock = -1;
+			continue;
+		}
+
+		break;
+	}
+	if (sock < 0) {
+		nvlist_add_string(nvlout, "errmsg", cause);
 		return (EINVAL);
-    }
-   
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
-    { 
-        nvlist_add_string(nvlout, "errmsg", "Connection failed.\n");
-		return (ECONNREFUSED);
-    }
+	}
+	freeaddrinfo(res0);
 	nvlist_move_descriptor(nvlout, "sockdesc", sock);
     return (0);
 }
